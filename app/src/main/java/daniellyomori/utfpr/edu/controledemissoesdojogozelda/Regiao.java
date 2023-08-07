5a64a65323cf3dc9e3531e841615657459ccd2c1
Danielly Omori <daniellyomori@MacBook-Pro-de-Danielly.local>
2023-08-06 22:09:01 -0300

    Adicionada activity contendo as regiões do céu e superfície

---
+++ app/src/main/java/daniellyomori/utfpr/edu/controledemissoesdojogozelda/Regiao.java
@@ -0,0 +1,45 @@
+package daniellyomori.utfpr.edu.controledemissoesdojogozelda;
+
+public class Regiao {
+    private String nome;
+    private String nomeTorre;
+    private Tipo tipo;
+
+    int qtdeShrines;
+
+    public Regiao(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeTorre() {
+        return nomeTorre;
+    }
+
+    public void setNomeTorre(String nomeTorre) {
+        this.nomeTorre = nomeTorre;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getQtdeShrines() {
+        return qtdeShrines;
+    }
+
+    public void setQtdeShrines(int qtdeShrines) {
+        this.qtdeShrines = qtdeShrines;
+    }
+}
